1ca74ba351ce6bc52361ea01cb09f7d55d12380a
Luke Slattery <lukeslattery@Lukes-MacBook-Pro.local>
2015-06-22 21:00:14 +0100

    0.2 - More Options

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # bump
-A simple, single command to open a web project in sublime text and your default browser.
+A simple, single command for mac to open a web project in sublime text and your default browser.
 
-*I don't think theres much point in versioning this but we'll call it 0.1.* :stuck_out_tongue_winking_eye:
+*I don't think theres much point in versioning this but we'll call it 0.2.* :stuck_out_tongue_winking_eye:
 ### Usage
 ```bash
 $ bump [options] [path to project directory]
 ```
 #### Options
 ##### -a
-Open all html files in the project in browser.
+Opens all html files in the project in browser.
 
 *By default bump will open the index file of the project in the browser.*
+##### -s
+Opens all files in Sublime Text and not the browser.
+##### -b
+Opens all files html in the browser and no files are opened Sublime Text.
 ##### *More options coming soon...*
 ### Set Up
 #### Prerequisites
--- src/bump.py
+++ src/bump.py
@@ -1,28 +1,44 @@
 #!/usr/bin/env python3
 
-# Bump 0.1
-# A simple, single command to open a web project in Sublime Text and your default browser.
+# Bump 0.2
+# A simgle, simple command to open your web projects in sublime text and your default browser
 
 from sys import argv, exit
 from subprocess import call
 import os.path
 from os import listdir
 
+package = ""
 packagePath = ""
 openAll = False
+openBrowser = True
+openSublime = True
 
-if argv[1] == "-a" :
-	packagePath = argv[2]
-	openAll = True
+if argv[1][0] != "-" :
+	package = argv[1]
+
 else :
-	packagePath = argv[1]
+	if "a" in argv[1] :
+		openAll = True
+	
+	if "b" in argv[1] :
+		openSublime = False
+	
+	if "s" in argv[1] :
+		openBrowser = False
+	
+	package = argv[2]
 
-if packagePath != "" :
+packagePath = package
+
+if packagePath != "" and openSublime :
 	call( ["subl", "%s" % ( packagePath )] )
+
+if openBrowser :
 	if os.path.isfile( "%s/index.html" % ( packagePath ) ) :
 		call( ["open", "%s/index.html" % ( packagePath )] )
 
-if openAll :
+if openAll and openBrowser :
 	for file in os.listdir( "%s" % ( packagePath ) ) :
 		if file.endswith( ".html" ) :
 			call( ["open", "%s/%s" % ( packagePath, file )] )
